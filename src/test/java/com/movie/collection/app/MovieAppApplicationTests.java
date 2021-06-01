f72c3fd0382ad810cc6dda23691a9454d8ef4711
Apocalypse <aduzgun99@gmail.com>
2021-06-01 03:02:05 +0300

    Initial commit

---
+++ src/test/java/com/movie/collection/app/MovieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.collection.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
